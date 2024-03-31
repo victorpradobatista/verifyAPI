306f8aa30e3b7488675804ca74d5abc33a0c30d6
PzDeveloper <129621220+PzDeveloper@users.noreply.github.com>
2024-03-30 23:25:59 -0300

    Initial commit

---
+++ config.lua
@@ -0,0 +1,5 @@
+cfg = {}
+
+cfg.license = "1CSN-QR7T-V4DX-14GJ"
+
+return cfg
---
+++ server.lua
@@ -0,0 +1,64 @@
+local env = module("umb_fac","config")
+
+local verify = 0
+
+Citizen.CreateThread(function()
+    while verify == 0 do
+        verify = verify + 1
+        local json = require("json")
+        local value = env.license
+        print(value)
+
+
+
+        PerformHttpRequest("http://localhost:3000/api/liscensesP", function(err, text, headers) 
+            local notLicense = '{"error":"Sem licensa!","result":"none"}'
+            
+            if err == 0 then
+                print("")
+                print("\27[37m[\027[31mumb_fac\27[37m] \27[37mScript \027[31mRecusado\27[37m Sem licensa!") -- Cinza, Amarelo, Cinza, Verde, Azul
+                print("[\27[38;5;229mDiscord\27[37m] https://discord.gg/umbrella")
+                Citizen.Wait(2000)
+                os.exit()
+            end
+            if text == notLicense then
+                print("")
+                print("\27[37m[\027[31mumb_fac\27[37m] \27[37mScript \027[31mRecusado\27[37m Sem licensa!") -- Cinza, Amarelo, Cinza, Verde, Azul
+                print("[\27[38;5;229mDiscord\27[37m] https://discord.gg/umbrella")
+                Citizen.Wait(5000)
+                os.exit()
+
+            else
+                Citizen.Wait(7000)
+                print("")
+                print("\27[37m[\027[94mumb_fac\27[37m] \27[37mScript \027[94mIniciada\27[37m com sucesso ") -- Cinza, Amarelo, Cinza, Verde, Azul
+                print("[\27[38;5;229mDiscord\27[37m] https://discord.gg/umbrella")
+            end
+        end, 'POST', value, { ['Content-Type'] = 'application/json', ['hux'] = value })
+
+		PerformHttpRequest("http://localhost:3000/api/liscenses", function(errorCode, resultData, resultHeaders)
+			local responseData = json.decode(resultData)
+            local resultData = tostring(resultData)
+            local notLicense = '{"error":"Sem licensa!","result":"none"}'
+            if errorCode == 0 then
+                print("")
+                print("\27[37m[\027[31mumb_fac\27[37m] \27[37mScript \027[31mRecusado\27[37m Sem licensa!") -- Cinza, Amarelo, Cinza, Verde, Azul
+                print("[\27[38;5;229mDiscord\27[37m] https://discord.gg/umbrella")
+                Citizen.Wait(2000)
+                os.exit()
+            end
+            if resultData == notLicense then
+                print("")
+                print("\27[37m[\027[31mumb_fac\27[37m] \27[37mScript \027[31mRecusado\27[37m Sem licensa!") -- Cinza, Amarelo, Cinza, Verde, Azul
+                print("[\27[38;5;229mDiscord\27[37m] https://discord.gg/umbrella")
+                Citizen.Wait(5000)
+                os.exit()
+            else
+                Citizen.Wait(7000)
+                print("")
+                print("\27[37m[\027[94mumb_fac\27[37m] \27[37mScript \027[94mIniciado\27[37m com sucesso ") -- Cinza, Amarelo, Cinza, Verde, Azul
+                print("[\27[38;5;229mDiscord\27[37m] https://discord.gg/umbrella")
+            end
+		end)
+    end
+end)
